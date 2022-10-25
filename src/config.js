162cb01ab96a38f578b78eefcef59040aba11d98
Danzsv <danzsv94@gmail.com>
2022-10-25 15:12:03 -0500

    new endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const apiUrl = "https://bsaleback-liodandev.up.railway.app/api";
+export const apiUrl = "https://bsale-test-back.onrender.com/api/";
 export const imgNotFound =
   "https://res.cloudinary.com/programandoandopf/image/upload/v1665728125/PF/imageNotFound_dfv8oy.png";
 export const searchIcon =
